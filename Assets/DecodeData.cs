43f2a5d70dde325a3b7e5bb99cb3fd638f5cfef5
Juan Blanco <juanfranblanco@hotmail.com>
2018-01-18 17:11:10 +0000

    initial commit

---
+++ Assets/DecodeData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using UnityEngine;
+using Nethereum.Contracts;
+using Nethereum.Web3;
+
+
+public class DecodeData : MonoBehaviour {
+
+   
+
+    void Start ()
+	{
+	    
+	}
+
+   
+    // Update is called once per frame
+    void Update ()
+    {
+
+        var web3 = new Web3();
+        Debug.Log(web3.Eth.Blocks.GetBlockNumber.SendRequestAsync().Result.Value.ToString());
+    }
+}
