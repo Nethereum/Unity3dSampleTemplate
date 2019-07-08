aeb6aaa11906583c0f1fbfea86fb0756a83bd448
Kevin Small <kevin.small@consensys.net>
2019-07-08 15:50:52 +0100

    Disable async controls in webgl

---
+++ Assets/ChangeTextInWebGL.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeTextInWebGL : MonoBehaviour
+{    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text text = GetComponent<Text>();
+#if UNITY_WEBGL
+        if (text != null)
+        {
+            text.text += "\nNot supported in WebGL";
+        }
+#endif        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/DisableInWebGL.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableInWebGL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+#if UNITY_WEBGL
+        if (canvasGroup != null)
+        {
+            canvasGroup.interactable = false;
+        }
+#endif        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
