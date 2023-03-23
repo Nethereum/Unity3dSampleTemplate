944dfb694b9794c2fba7cc4acff0168264940d58
Juan Blanco <juanfranblanco@hotmail.com>
2023-03-23 09:45:57 +0000

    Web3 / Task ERC20 example, increase --generic-virtual-method-iterations=5 to ensure everything is included in AOT

---
+++ Assets/Editor/SetIl2cppSettings.cs
@@ -0,0 +1,16 @@
+﻿
+
+    using UnityEditor;
+    using UnityEditor.Build;
+    using UnityEditor.Build.Reporting;
+
+    public class SetIl2cppSettings : IPreprocessBuildWithReport
+    {
+        public int callbackOrder => 0;
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            PlayerSettings.SetAdditionalIl2CppArgs("--generic-virtual-method-iterations=5");
+        }
+    }
+
