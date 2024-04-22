e41b25880caf211d9afcd40b4be265ffc67466c2
Juan Blanco <juanfranblanco@hotmail.com>
2024-04-22 14:32:00 +0100

    ChainId and test update

--- Sample.Unity/Assets/TokenDeployAndSendCoroutinesUnityWebRequest.cs
+++ Sample.Unity/Assets/TokenDeployAndSendCoroutinesUnityWebRequest.cs
@@ -81,7 +81,7 @@ public partial class TokenDeployAndSendCoroutinesUnityWebRequest : MonoBehaviour
         var privateKey = "0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7";
         var account = "0x12890d2cce102216644c59daE5baed380d84830c";
         //initialising the transaction request sender
-        var transactionRequest = new TransactionSignedUnityRequest(url, privateKey, 444444444500);
+        var transactionRequest = new TransactionSignedUnityRequest(url, privateKey, 31337);
         transactionRequest.UseLegacyAsDefault = true;
 
 
